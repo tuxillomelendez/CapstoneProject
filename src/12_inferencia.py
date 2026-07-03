8c9322ab0ad0286aa87738061d7204587d97f215
tuxillomelendez <melendez.jorge@gmail.com>
2026-07-03 10:35:42 -0400

    Update university location in comments

--- src/12_inferencia.py
+++ src/12_inferencia.py
@@ -6,7 +6,7 @@ Autor:             Jorge Eduardo Meléndez Bastías
 Profesor guía:     Dr. Christian Pieringer Baeza
 Profesor co-guía:  Dr. Ronal Manuel Coronado
 Profesor revisor:  Francisco Pérez Galarce
-Universidad de Las Américas (UDLA), Santiago de Chile, 2026.
+Universidad de Las Américas (UDLA), Chile, 2026.
 
 Script 12 - Inferencia y priorización de equipos por riesgo.
 
